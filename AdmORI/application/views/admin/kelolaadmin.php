c5ea058818d4189d5c3f7fcc04855ee9f89b8599
ImamHaris <iamimam0107@gmail.com>
2019-06-11 09:04:45 +0700

    Update kelolaadmin.php

--- AdmORI/application/views/admin/kelolaadmin.php
+++ AdmORI/application/views/admin/kelolaadmin.php
@@ -43,47 +43,53 @@
 <!-- main-panel ends -->
 
 <!-- MODAL ADD -->
-
-        <form id="form_admin">
+<form id="form_admin">
         <div class="modal fade" id="Modal_Add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Admin Baru</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2 col-form-label">Nama</label>
-                        <div class="col-md-10">
-                          <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama" required>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2 col-form-label">Username</label>
-                        <div class="col-md-10">
-                          <input type="text" name="username" id="username" class="form-control" placeholder="Nama" required>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2 col-form-label">Password</label>
-                        <div class="col-md-10">
-                          <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
-                        </div>
-                    </div>
-                    
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" type="submit" id="btn_save" class="btn btn-primary">Save</button>
-              </div>
-            </div>
-          </div>
+
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Tambah Admin Baru</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <i class="fas fa-user prefix grey-text"></i>
+          <label data-error="wrong" data-success="right" for="orangeForm-name">Nama</label>
+          <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama" required>   
         </div>
-        </form>
+        <div class="md-form mb-5">
+          <i class="fas fa-envelope prefix grey-text"></i>
+          <label data-error="wrong" data-success="right" for="orangeForm-email">Username</label>
+          <input type="text" name="username" id="username" class="form-control" placeholder="Nama" required>         
+        </div>
+
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text"></i>
+          <label data-error="wrong" data-success="right" for="orangeForm-pass">Password</label>
+          <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" type="submit" id="btn_save" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="text-center">
+  <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modalRegisterForm">Launch
+    Modal Register Form</a>
+</div>
+
+
+
+
+        
     <!--END MODAL ADD-->
 
     <!-- MODAL EDIT -->
